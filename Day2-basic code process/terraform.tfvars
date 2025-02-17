b1e47872d9b76c9f3e49f8a13c35b8b688bbd0c7
unknown <Nazz>
2025-02-17 19:30:28 +0530

    Day 1,2

---
+++ Day2-basic code process/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_id = "ami-053a45fff0a704a4"
+instance_type = "t2.micro"
+key_name = "newkey"
+
---
+++ Day2-basic code process/variable.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+  default     = "ami-053a45fff0a704a4"
+}
+
+variable "instance_type" {  # Renamed from 'type' to 'instance_type' for clarity
+  description = "The type of instance to launch"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {  # Renamed from 'key' to 'key_name' for clarity
+  description = "The name of the SSH key pair to use"
+  type        = string
+  default     = "newkey"
+}
